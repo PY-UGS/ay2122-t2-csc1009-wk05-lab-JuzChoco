01d091d102117808e7e48e436a06f961ce813ea2
JuzChoco <97875095+JuzChoco@users.noreply.github.com>
2022-03-01 11:58:43 +0800

    Qns1

---
+++ TestCircleRectangle.java
@@ -0,0 +1,19 @@
+public class TestCircleRectangle {
+    
+    public static void main(String[] args){
+
+        Circle circle = new Circle(1);
+
+        System.out.println("The circle is " + circle.toString());
+        System.out.println("The radius is " + circle.getColor());
+        System.out.println("The radius is " + circle.getRadius());
+        System.out.println("The area is " + circle.getArea());
+        System.out.println("The diameter is " + circle.getDiameter());   
+        
+        Rectangle rect = new Rectangle(2, 4);
+
+        System.out.println("\nA rectangle " + rect.toString());
+        System.out.println("\nThe area is " + rect.getArea());
+        System.out.println("\nThe perimeter is " + rect.getPerimeter());
+    }   
+}
